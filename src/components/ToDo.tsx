c7ae8c977db65fb2593e0a2cff18a894412d949d
Daniel Chung <daniel20020228@gmail.com>
2024-12-24 13:16:23 +1000

    add selector that allows you to sort the list by its catogories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDo } from "../atom";
+import { IToDo, toDoState } from "../atom";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const newToDo = { text, id, category: name as any };
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                newToDo,
+                ...oldToDos.slice(targetIndex + 1),
+            ];
+        });
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>Doing</button>
-            <button>To Do</button>
-            <button>Done</button>
+            {category !== "DOING" && (
+                <button name='DOING' onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "TO_DO" && (
+                <button name='TO_DO' onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name='DONE' onClick={onClick}>
+                    Done
+                </button>
+            )}
         </li>
     );
 }
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,21 +1,37 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atom";
+import { toDoSelector } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-    const todos = useRecoilValue(toDoState);
+    const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Do List</h1>
             <hr></hr>
             <CreateToDo />
+            <h2>To Dos</h2>
             <ul>
-                {todos.map((todo) => (
+                {toDo.map((todo) => (
                     <ToDo key={todo.id} {...todo} />
                 ))}
             </ul>
+            <hr />
+            <h2>Doings</h2>
+            <ul>
+                {doing.map((doing) => (
+                    <ToDo key={doing.id} {...doing} />
+                ))}
+            </ul>
+            <hr />
+            <h2>Dones</h2>
+            <ul>
+                {done.map((done) => (
+                    <ToDo key={done.id} {...done} />
+                ))}
+            </ul>
+            <hr />
         </div>
     );
 }
