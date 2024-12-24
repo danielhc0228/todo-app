64c637d7fd379a3e08d7f40360cb3b1dfd76cd1e
Daniel Chung <daniel20020228@gmail.com>
2024-12-24 14:29:44 +1000

    add enum in atom and modify other files accordingly

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { categoryState, toDoSelector } from "../atom";
+import { Categories, categoryState, toDoSelector } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,16 +7,17 @@ function ToDoList() {
     const toDos = useRecoilValue(toDoSelector);
     const [category, setCategory] = useRecoilState(categoryState);
     const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-        setCategory(event.currentTarget.value);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        setCategory(event.currentTarget.value as any);
     };
     return (
         <div>
             <h1>To Do List</h1>
             <hr></hr>
             <select value={category} onInput={onInput}>
-                <option value='TO_DO'>To Do</option>
-                <option value='DOING'>Doing</option>
-                <option value='DONE'>Done</option>
+                <option value={Categories.TO_DO}>To Do</option>
+                <option value={Categories.DOING}>Doing</option>
+                <option value={Categories.DONE}>Done</option>
             </select>
             <CreateToDo />
             {toDos?.map((toDo) => (
